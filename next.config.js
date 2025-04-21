6465b31a0c6e9c1a17cd273c09f5bdcd525e2f05
Moalea Adrian <tytygelu@yahoo.com>
2025-04-22 00:03:30 +0300

    fix(config): dezactivează experimental.mdxRs pentru suport tabele Markdown

--- next.config.js
+++ next.config.js
@@ -8,9 +8,9 @@ const withMDX = require('@next/mdx')({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
-  experimental: {
-    mdxRs: true,
-  },
+  // experimental: {
+  //   mdxRs: true,
+  // },
 }
 
 module.exports = withMDX(nextConfig)
